bf2e7263d33f137983b1b0f4b4a9a4b91eb55e63
Gabriel Braga <kevencript@gmail.com>
2019-09-15 08:05:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
